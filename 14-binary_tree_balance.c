8159b883cad29a293a4ddceabba11a980a451f17
Christian Timbe <bobitlmr188221@spu.ac.ke>
2024-01-04 09:57:18 +0300

    Complete task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,3 +16,22 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
 
+/**
+ * binary_tree_height - This function measures the height of a binary tree
+ * @tree: This is a pointer to the root node of the tree
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (!tree)
+		return (0);
+
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((left > right) ? left : right);
+}
