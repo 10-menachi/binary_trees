cccbf964db93121e56043c6ba79e6fb567ae981d
Christian Timbe <bobitlmr188221@spu.ac.ke>
2024-01-04 09:42:53 +0300

    Fix bug in code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left, right;
+	size_t left = 0;
+	size_t right = 0;
 
 	if (!tree)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
-
-	return ((left > right ? left : right) + 1);
+	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return ((left > right) ? left : right);
 }
